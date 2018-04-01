1af907b7d8c7cb756980c777c08f04149c46925d
Joaquin Fernandez <joaquin@asperasoft.com>
2018-04-02 00:27:48 +0200

    Added more metrics and example data

--- webapp/controller/truck.js
+++ webapp/controller/truck.js
@@ -1,3 +1,49 @@
+var lastStopTime = new Date()
+var lastStop = [0,0,0,0]
+var lastStopDuration = 0
+
+function printTime(time) {
+  return time.getDate() + "/"
+          + (time.getMonth()+1)  + "/" 
+          + time.getFullYear() + " @ "  
+          + ('0' + time.getHours()).substr(-2) + ":"  
+          + ('0' + time.getMinutes()).substr(-2);
+}
+
+function printStopDuration(duration) {
+  var ms = duration % 1000;
+  duration = (duration - ms) / 1000;
+  var secs = duration % 60;
+  duration = (duration - secs) / 60;
+  var mins = duration % 60;
+  var hrs = (duration - mins) / 60;
+
+  return ('0' + hrs).substr(-2) + ':' + ('0' + mins).substr(-2) + ':' + ('0' + secs).substr(-2);
+}
+
+function calculateLastStop(payload) {
+  var accelX = parseFloat(payload["accelX"])
+  var accelY = parseFloat(payload["accelY"])
+  var accelZ = parseFloat(payload["accelZ"])
+  
+  if (accelX != lastStop[0] || accelY != lastStop[1] || accelZ != lastStop[2]) {
+    if (lastStop[3] == 0) {
+      lastStop[3] = 1
+    }
+    lastStop[0] = accelX
+    lastStop[1] = accelY
+    lastStop[2] = accelZ
+  } else {
+    if (lastStop[3] == 1) {
+      lastStopTime = new Date()
+      lastStop[3] = 0
+    } else {
+      lastStopDuration = Math.abs(new Date() - lastStopTime.getTime());
+    }
+  } 
+}
+
+
 var wsbroker = "iot.eclipse.org";  //mqtt websocket enabled broker
 var wsport = 443 // port for above
 var client = new Paho.MQTT.Client(wsbroker, wsport, "myclientid_" + parseInt(Math.random() * 100, 10));
@@ -5,11 +51,15 @@ client.onConnectionLost = function (responseObject) {
   console.log("connection lost: " + responseObject.errorMessage);
 };
 client.onMessageArrived = function (message) {
-  var payload = JSON.parse(message.payloadString)
-  $("#ambientTemp").text("Temperature: " + parseFloat(payload["messages"][0]["ambientTemp"]).toFixed(2) + "°C")
-  $("#pressure").text("Pressure: " + parseFloat(payload["messages"][0]["pressure"]).toFixed(2) + " bars")
-  $("#altitude").text("Altitude: " + parseFloat(payload["messages"][0]["altitude"]).toFixed(2) + " meters")
-  $("#light").text("Light: " + parseFloat(payload["messages"][0]["light"]).toFixed(2) + " lumens")
+  var payload = JSON.parse(message.payloadString)["messages"][0]
+  calculateLastStop(payload)
+  $("#deviceID").text("Device ID: " + payload["sensor"])
+  $("#ambientTemp").text("Temperature: " + parseFloat(payload["ambientTemp"]).toFixed(2) + "°C")
+  $("#pressure").text("Pressure: " + parseFloat(payload["pressure"]).toFixed(2) + " mbar")
+  $("#altitude").text("Altitude: " + parseFloat(payload["altitude"]).toFixed(2) + " meter")
+  $("#light").text("Light: " + parseFloat(payload["light"]).toFixed(2) + " lux")
+  $("#lastStop").text("Last stop: " + printTime(lastStopTime))
+  $("#lastStopDuration").text("Duration: " + printStopDuration(lastStopDuration))
   console.log(message.destinationName, ' -- ', message.payloadString);
 };
 var options = {
